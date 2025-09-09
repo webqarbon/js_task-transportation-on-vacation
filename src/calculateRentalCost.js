2e88a39df52fdfc64caa16fa87c98b0b7ea6e8d7
Dmytro Kutsenko <dmytrokutsenkodev@gmail.com>
2025-09-09 15:01:46 +0300

    fix solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -14,17 +14,15 @@ function calculateRentalCost(days) {
 
   const totalCost = days * RENT_VALUE;
 
-  if (days < SHORT_TERM) {
-    return totalCost;
-  }
-
-  if (days < LONG_TERM) {
-    return totalCost - SHORT_TERM_DISCOUNT;
-  }
-
   if (days >= LONG_TERM) {
     return totalCost - LONG_TERM_DISCOUNT;
   }
+
+  if (days >= SHORT_TERM) {
+    return totalCost - SHORT_TERM_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
